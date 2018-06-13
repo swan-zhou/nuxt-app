c18ba20d34c8b861413cd2c7aa62c10787ad45c6
zhoudan <zhoudan@vite.org>
2018-06-13 14:47:30 +0800

    app init

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  head: {
+    title: '{{ name }}',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#3B8070' },
+}
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>test {{name}}</span>
+</template>
+
+<script>
+export default {
+  asyncData (ctx) {
+    return { name: 'world'}
+  },
+  data () {
+    return {
+      title: 'hahha'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+    }
+  }
+}
+</script>
